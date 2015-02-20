96fe3259ef9e06c0ccb931da7a1de7834744f829
Josh Cai <yuanshi419@gmail.com>
2015-02-19 20:14:32 -0600

    add decorator to restrict based on role

---
+++ app/decorators.py
@@ -0,0 +1,17 @@
+from functools import wraps
+from flask_login import current_user
+from flask import flash, redirect, url_for
+
+def role_required(required_role):
+  def wrapper(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+      if not current_user.has_role(required_role):
+          # Flash unauthorized view and redirect home
+          flash('You are not authorized to view that page')
+          return redirect(url_for('index'))
+
+      # Call the actual view
+      return func(*args, **kwargs)
+    return decorated_view
+  return wrapper
